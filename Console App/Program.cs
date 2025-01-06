919ee517585404165a1aab11a20615b4270b6caf
Dmytro Marilovtsev <dimarilovcev@gmail.com>
2025-01-06 16:43:42 +0200

    First commit 'Hello World'

--- Console App/Program.cs
+++ Console App/Program.cs
@@ -10,6 +10,8 @@ namespace Console_App
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
